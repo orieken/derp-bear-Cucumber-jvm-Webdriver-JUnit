c30d07c9033b53520eb275d990029a7363eba223
Oscar Rieken <oriekenjr@gmail.com>
2013-06-02 23:34:18 -0400

    added element to allow going to any example updated the stepdefs to reflect this 2 passing features now

--- src/main/java/derpbear/LandingPage.java
+++ src/main/java/derpbear/LandingPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
  * User: bisbot
  * Date: 6/2/13
  * Time: 4:04 PM
- * To change this template use File | Settings | File Templates.
+ * Landing page for http://derp-bear.herokuapp.com/
  */
 public class LandingPage extends Page{
 
@@ -21,8 +21,17 @@ public class LandingPage extends Page{
 
 
     public LandingPage examplesList(){
-        driver.findElement(exampleListLocator);
+        driver.findElement(exampleListLocator).click();
         return this;
+    }
+
+    // taking a stab at one method to pick an example since they all
+    //follow the same pattern "some_name_example"
+
+    public LandingPage pickExample(String exampleName) {
+        String modifiedExampleName = exampleName.replaceAll(" ", "_").toLowerCase().concat("_example");
+        driver.findElement(By.id(modifiedExampleName));
+        return new LandingPage(driver);
     }
 
 }
--- src/test/java/derpbear/LandingPageStepdefs.java
+++ src/test/java/derpbear/LandingPageStepdefs.java
@@ -1,8 +1,15 @@
 package derpbear;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 import cucumber.api.PendingException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.matchers.JUnitMatchers;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,18 +20,47 @@ import cucumber.api.PendingException;
  */
 
 public class LandingPageStepdefs {
+    private WebDriver driver;
+    private String derpBearUrl;
 
+//hooks i want to move this later
+    @Before
+    public void setUp() {
+        derpBearUrl = "http://derp-bear.herokuapp.com/";
+        driver = new FirefoxDriver();
+    }
 
+    @After
+    public void tearDown() {
+        if (driver != null){
+            driver.close();
+        }
+    }
 
+//steps
     @When("^I visit Derp-Bear$")
     public void I_visit_Derp_Bear() throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        driver.navigate().to(derpBearUrl);
     }
 
     @Then("^I should see a list of available examples$")
     public void I_should_see_a_list_of_available_examples() throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        LandingPage landingPage = new LandingPage(driver);
+        assertThat(landingPage.examplesList(), is(notNullValue()));
     }
+
+    @When("^I pick a basic example$")
+    public void I_pick_a_basic_example() throws Throwable {
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.pickExample("Basic Form");
+    }
+
+    @Then("^I should see be on the basic example page$")
+    public void I_should_see_be_on_the_basic_example_page() throws Throwable {
+        LandingPage landingPage = new LandingPage(driver);
+        String pageTitle = landingPage.driver.getTitle();
+        assertThat(pageTitle, is(equalTo("Welcome to Derp-Bear")));
+    }
+
+
 }
